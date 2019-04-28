b839a38b91e84d7eaef81d97bcc27f5273637763
Dirk Kelly <github@dirkkelly.com>
2019-04-28 11:22:01 +0800

    refactor CreateChannelVideoFolder

--- util/videos.go
+++ util/videos.go
@@ -9,14 +9,20 @@ import (
 	"strings"
 )
 
+func CreateChannelVideoFolder(channel *Channel, projectRoot string) error {
+	folder := path.Join(projectRoot, fmt.Sprintf("/data/videos/%s", channel.Slug))
+
+	// Make a video directory with a .gitignore
+	videoFolder := os.Mkdir(folder, os.ModePerm)
+	// There was a suggestion to gitignore, but when importing I like to immediately start committing videos
+	os.OpenFile(path.Join(folder, "TODO.yml"), os.O_RDONLY|os.O_CREATE, 0666)
+
+	return videoFolder
+}
+
 // GetCreatorVideos returns a list of video IDs for a given creator
 func GetCreatorVideos(slug string, projectRoot string) ([]string, error) {
 	folder := path.Join(projectRoot, fmt.Sprintf("/data/videos/%s", slug))
-
-	// Make a video directory with a .gitignore
-	os.Mkdir(folder, os.ModePerm)
-	// There was a suggestion to gitignore, but when importing I like to immediately start committing videos
-	// os.OpenFile(path.Join(folder, ".gitignore"), os.O_RDONLY|os.O_CREATE, 0666)
 
 	// Using "channels" for consistency, but will change to creators in refactor.
 	videoDir, err := ioutil.ReadDir(folder)
