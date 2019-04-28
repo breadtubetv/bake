42695ad87d58c3547a02abba240d1fa2acf79ced
Dirk Kelly <github@dirkkelly.com>
2019-04-28 11:11:02 +0800

    Create Video Dir

--- util/videos.go
+++ util/videos.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path"
 	"path/filepath"
 	"strings"
@@ -10,8 +11,15 @@ import (
 
 // GetCreatorVideos returns a list of video IDs for a given creator
 func GetCreatorVideos(slug string, projectRoot string) ([]string, error) {
+	folder := path.Join(projectRoot, fmt.Sprintf("/data/videos/%s", slug))
+
+	// Make a video directory with a .gitignore
+	os.Mkdir(folder, os.ModePerm)
+	// There was a suggestion to gitignore, but when importing I like to immediately start committing videos
+	// os.OpenFile(path.Join(folder, ".gitignore"), os.O_RDONLY|os.O_CREATE, 0666)
+
 	// Using "channels" for consistency, but will change to creators in refactor.
-	videoDir, err := ioutil.ReadDir(path.Join(projectRoot, fmt.Sprintf("/data/videos/%s", slug)))
+	videoDir, err := ioutil.ReadDir(folder)
 	if err != nil {
 		return nil, err
 	}
