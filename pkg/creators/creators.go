e617ee1d48ce9a8f66c7866266f4a07a0608d236
Josh Michielsen <github@mickey.dev>
2019-07-01 00:36:29 +0100

    Plan Creator API

--- pkg/creators/creators.go
+++ pkg/creators/creators.go
@@ -10,25 +10,26 @@ import (
 // Creator refers to a single BreadTube creator.
 // A creator can have many providers.
 type Creator struct {
-	name      string
-	permalink string
-	slug      string
-	tags      []string
-	Providers providers.ProviderI
-	Provider  map[string]providers.Provider
-}
-
-var config *viper.Viper
-
-// AddConfig allows package user to pass a viper configuation to the pkg
-func AddConfig(conf *viper.Viper) error {
-	config = conf
-	return nil
+	Name      string
+	Permalink string
+	Slug      string
+	Tags      []string
+	Providers providers.Providers
 }
 
 // FindCreatorBySlug retreives the creator based on the provided
 // slug, loads the creator, and returns its creator object.
 func FindCreatorBySlug(slug string) *Creator {
 	fmt.Print(slug)
+	fmt.Println(viper.GetString("projectRoot"))
 	return &Creator{}
 }
+
+func NewCreator(name, slug string) (*Creator, error) {
+	// Check if creator already exists
+	// Generate Permalink
+	return nil, nil
+}
+
+// func (c *Creator) LoadProviders() *Creator
+// func (c *Creator) AddTag(tag string) *Creator
